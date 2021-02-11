b3867e940fe7a67c1d96725fa730a994bca7e567
RJ Wright <rj@rjwright.me>
2021-02-11 21:54:38 +1100

    Remove CRACO because of build issues. Replace with PostCSS.

---
+++ tailwind.js
@@ -0,0 +1,41 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.jsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      red: colors.red,
+      yellow: colors.yellow,
+      green: {
+        light: "#58DAB4",
+        DEFAULT: "#4DCEA8",
+      },
+      blue: "#2FA8B8",
+      gray: colors.gray,
+      silver: "#F5F7FB",
+      aqua: {
+        light: "#57DDD5",
+        DEFAULT: "#58D6D2",
+        dark: "#5DC1CF",
+      },
+      ocean: {
+        DEFAULT: "#2FA8B8",
+        dark: "#0c4a52",
+      },
+    },
+    fontFamily: {
+      sans: ["rubik", ...defaultTheme.fontFamily.sans],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
